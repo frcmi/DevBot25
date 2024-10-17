32cda3beda46a09b3ebdd6ed0e4d39f31c29a2dc
Nora Beda <theo.github@bedafamily.com>
2024-10-17 16:47:19 -0700

    add default command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,11 +4,24 @@
 
 package frc.robot;
 
+import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.DriveRequestType;
+
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.SwerveSubsystem;
 
 public class RobotContainer {
+  private static final double kMaxVelocity = 4.73; // m/s
+  private static final double kMaxAngularVelocity = 1.5 * Math.PI; // rad/s
+
+  private CommandXboxController m_Controller;
+
   private SwerveSubsystem m_Swerve;
+  private SwerveRequest.FieldCentric m_Request = new SwerveRequest.FieldCentric()
+    .withDeadband(kMaxVelocity * 0.1)
+    .withRotationalDeadband(kMaxVelocity * 0.1)
+    .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
 
   public RobotContainer() {
     initSubsystems();
@@ -19,7 +32,22 @@ public class RobotContainer {
     m_Swerve = SwerveSubsystem.configure();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    m_Controller = new CommandXboxController(0);
+
+    m_Swerve.setDefaultCommand(m_Swerve.applyRequest(() -> m_Request
+      // stick up is -1
+      // field x axis is along the length of the field
+      .withVelocityX(-m_Controller.getLeftY() * kMaxVelocity)
+      
+      // stick right is +1
+      // y axis is to the left
+      .withVelocityY(-m_Controller.getLeftY() * kMaxVelocity)
+
+      // stick right is +1
+      // +theta is counterclockwise
+      .withRotationalRate(-m_Controller.getRightX() * kMaxAngularVelocity)));
+  }
 
   public Command getAutonomousCommand() {
     return null;
